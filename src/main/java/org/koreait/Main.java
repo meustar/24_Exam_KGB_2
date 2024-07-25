9bd5993e8b630b478c04db55d67c346f673ebdf1
meustar <meustar829@gmail.com>
2024-07-25 17:07:10 +0900

    1문제 완료.

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,7 +1,23 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("가로길이, 세로길이 입력받아 사각형 넓이만큼 별찍기.");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("가로, 세로 길이 입력해주세요: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        for (int i = 1; i <= n2; i++) {
+            for (int j = 1; j <= n1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+
     }
 }
