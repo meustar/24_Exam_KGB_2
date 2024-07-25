dbb6eeedca1c64b56b6109a6fe258d91ecad3bfa
meustar <meustar829@gmail.com>
2024-07-25 17:57:20 +0900

    1,2,3번 제출

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -4,19 +4,51 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("가로길이, 세로길이 입력받아 사각형 넓이만큼 별찍기.");
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("가로, 세로 길이 입력해주세요: ");
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
+//        // 1번 문제.
+//        System.out.println("1. 가로길이, 세로길이 입력받아 사각형 넓이만큼 별찍기.");
+//
+//        System.out.print("가로, 세로 길이 입력해주세요: ");
+//        int n1 = sc.nextInt();
+//        int n2 = sc.nextInt();
+//
+//        for (int i = 1; i <= n2; i++) {
+//            for (int j = 1; j <= n1; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
 
-        for (int i = 1; i <= n2; i++) {
-            for (int j = 1; j <= n1; j++) {
-                System.out.print("*");
+        // 2번 문제.
+        System.out.print("2. 정수를 입력해주세요. : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                System.out.print(" " + j + " ");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
+
+
+        // 3번 문제.
+//        System.out.println("3. 정수 10개의 피보나치 수열을 출력한다.");
+//
+//        int num1 = 0;
+//        int num2 = 1;
+//        int sum = 0;
+//
+//        System.out.print(num1+ " ");
+//        System.out.print(num2 + " ");
+//        // f(n) = (n-1) +
+//        for (int i = 2; i < 10; i++) {
+//            sum = num1 + num2;
+//            System.out.print(sum + " ");
+//
+//            num1 = num2;
+//            num2 = sum;
+//
+//        }
 
 
     }
